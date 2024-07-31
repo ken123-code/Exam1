d376041dc46e4fb0f80f600d4f34f6ff3977bb95
ken123-code <kennywolf35@gmail.com>
2024-07-31 15:26:27 +0700

    sua lan 1

--- src/components/ChemicalApp.js
+++ src/components/ChemicalApp.js
@@ -13,16 +13,14 @@ const [list, setList] = useState([
   const deleteById = (id) => {
     setList(list.filter((chemical) => chemical.id !== id));
   };
-  const reChecked = (id) => {
-    let newList = list.map((chemical) =>
-      chemical.id === id ? { ...chemical, checked: !chemical.checked } : chemical
-    );
-    setList(newList);
-  };
   const renameChemical = (id, name) => {
+    let Arr = name.split(" ");
     let newList = list.map((chemical) =>
-      chemical.id === id ? { ...chemical, name: name } : chemical
-    );
+        chemical.id === id ? { ...chemical, name: Arr[0] , formula:Arr[1] } : chemical
+      );
+    // let newList = list.map((chemical) =>
+    //   chemical.id === id ? { ...chemical, name: name } : chemical
+    // );
     setList(newList);
   };
   const addNewChemical = (name) => {
@@ -30,13 +28,26 @@ const [list, setList] = useState([
       (max_value, chemical) => Math.max(max_value, chemical.id),
       -Infinity
     );
-    setList([...list, { id: list.length == 0 ? 1 : idMax + 1, name: name }]);
+    let Arr = name.split(" ");
+    setList([...list, { id: list.length == 0 ? 1 : idMax + 1, name: Arr[0], formula:Arr[1] }]);
   };
   return (
     <div>
       <Container className="w-50 text-center p-5 my-5">
-        <h1>Chemical List</h1>
+        <h1 className="h1">Chemical List</h1>
+        {/* <Input
+          className="my-2 px-4 "
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key == "Enter") {
+                addNewChemical(text);
+                setText("");
+            }
+          }}
+        /> */}
         <Input
+        placeholder="Them theo cu phap:  [Ten chat moi] [Khoang trang] [Cong thuc hoa hoc]"
           className="my-2 px-4 "
           value={text}
           onChange={(e) => setText(e.target.value)}
@@ -53,12 +64,13 @@ const [list, setList] = useState([
         <div>
 
         {
-            !isEdit?<p onDoubleClick={()=>setIsEdit(true)}>{item.name}</p>:
+            !isEdit?<p onDoubleClick={()=>setIsEdit(true)}>{item.id} - {item.name} - {item.formula}</p>:
             <Input
                 value={text} onChange={(e)=>setText(e.target.value)} onKeyDown={(e)=>{
                     if(e.key=="Enter"){
                         setIsEdit(false)
                         renameChemical(item.id,text)
+                        setText("");
                     }
                 }}
             />
